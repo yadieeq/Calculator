4e3f19f34c93cb9ae13ba98c5693f048801c491b
Danylo Mazur <danmaz4343@gmail.com>
2025-09-20 14:07:16 +0200

    add operator handling in calculator

--- script.js
+++ script.js
@@ -16,59 +16,146 @@ let num2 = 0
 let operation = ''
 let result = 0
 
-// Main functions
-window.onload = () => {inpNum.focus()}
-
+// Logical
 const setOperation = (op, symbol) => {
-    num1 = +inpNum.value;       
-    operation = op;              
-    inpNum.value = '';           
-    out.innerHTML += '<br>' + num1 + ' ' + symbol + ' ';
-    inpNum.focus();               
+    num1 = +inpNum.value;
+    operation = op;
+    inpNum.value = '';;
+    inpNum.focus();
+}
+const disableBtn = () => {
+    btnPlus.disabled = true
+    btnMinus.disabled = true
+    btnMultiply.disabled = true
+    btnDivide.disabled = true
 }
 
-btnClear.onclick = function (){
-    num1 = 0
-    num2 = 0
-    result = 0
-    out.textContent = ''
-    inpNum.value = 0
-    inpNum.focus()
+const changeActiveBtn = (is) => {
+    btnPlus.disabled = is
+    btnMinus.disabled = is
+    btnMultiply.disabled = is
+    btnDivide.disabled = is
 }
 
+const disableEquals = () => {
+    btnEquals.disabled = true
+}
+
+const enableEquals = () => {
+    if (operation !== '') btnEquals.disabled = false
+}
+
+// Events
 inputNumBtns.forEach(btn => {
-        btn.onclick = () => {
-        inpNum.value += btn.textContent // добавляем цифру в конец
-        inpNum.focus() // оставляем курсор в инпуте
+    btn.onclick = () => {
+        inpNum.value += btn.textContent
+        inpNum.focus()
+        enableEquals()
     }
 })
 
-btnPlus.onclick = () => setOperation('+', '+');
-btnMinus.onclick = () => setOperation('-', '-');
-btnMultiply.onclick = () => setOperation('*', 'x');
-btnDivide.onclick = () => setOperation('/', '÷');
+btnPlus.onclick = () => {
+    setOperation('+', '+');
+    disableBtn()
+}
+btnMinus.onclick = () => {
+    setOperation('-', '-');
+    disableBtn()
 
-btnEquals.onclick = function (){
+}
+btnMultiply.onclick = () => {
+    setOperation('*', 'x');
+    disableBtn()
+
+}
+btnDivide.onclick = () => {
+    setOperation('/', '÷');
+    disableBtn()
+
+}
+
+const equalsFunc = () => {
     num2 = +inpNum.value
-    
-    switch(operation) {
-        case '+' : result = num1 + num2; break;
-        case '-' : result = num1 - num2; break;
-        case '*' : result = num1 * num2; break;
-        case '/' :
+    let visualOperation = ''
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            visualOperation = '+';
+            break;
+        case '-':
+            result = num1 - num2;
+            visualOperation = '-';
+            break;
+        case '*':
+            result = num1 * num2;
+            visualOperation = 'x';
+            break;
+        case '/':
             if (num2 === 0) {
                 out.innerHTML += ' ' + num2 + ' = Impossible';
                 inpNum.value = 0;
             } else {
                 result = num1 / num2;
+                visualOperation = '÷';
             }
-        break;
+            break;
     }
 
-    out.innerHTML += ' ' + num2 + ' = ' + result + '<br>'; inpNum.value = result
+    out.innerHTML += num1 + ' ' + ' ' + visualOperation + ' ' + num2 + ' = ' + +result.toFixed(4) + ' <br>';
+    inpNum.value = +result.toFixed(4)
     inpNum.focus()
+
+    changeActiveBtn(false)
+    disableEquals()
+    operation = ''
+
 }
 
+btnEquals.onclick = equalsFunc
 
 
+btnClear.onclick = function () {
+    num1 = 0
+    num2 = 0
+    result = 0
+    out.textContent = ''
+    inpNum.value = ' '
+    inpNum.focus()
+    changeActiveBtn(false)
+    disableEquals()
+}
 
+const checkInput = (key) => {
+    return ["+", "-", "*", "/"].includes(key)
+}
+
+const checkOperatorIndex = (value, key) => {
+    return value.indexOf(key)
+}
+
+inpNum.onkeyup = (e) => {
+    if (checkInput(e.key)) {
+        const index = checkOperatorIndex(inpNum.value, e.key);
+
+        num1 = +inpNum.value.slice(0, index)
+        operation = e.key
+
+        // console.log("Индекс знака:", index, "num1", num1, operation)
+        inpNum.value = ''
+
+        
+    }
+    if(e.key === "Enter") {
+        if(operation !== '') equalsFunc()
+    }
+}
+
+inpNum.oninput = () => {
+    enableEquals()
+}
+
+//Start
+window.onload = () => {
+    inpNum.focus()
+}
